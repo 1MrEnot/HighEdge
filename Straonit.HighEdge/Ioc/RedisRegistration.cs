3b64434bdcc6d9063d7e63aa1078e824d80afcf6
maxim.eremeev <maxim9236@yandex.ru>
2022-03-12 19:36:38 +0300

    Removes ssl for redis

--- Straonit.HighEdge/Ioc/RedisRegistration.cs
+++ Straonit.HighEdge/Ioc/RedisRegistration.cs
@@ -9,13 +9,9 @@ public static class RedisRegistration
     public static IServiceCollection AddRedisDatabase(this IServiceCollection serviceCollection,
         ConfigurationManager config)
     {
-        serviceCollection.AddSingleton<IConnectionMultiplexer>(sp => ConnectionMultiplexer.Connect(
-            new ConfigurationOptions
-            {
-                EndPoints = { $"{config.GetValue<string>("Redis:Host")}:{config.GetValue<int>("Redis:Port")}" },
-                Ssl = true,
-                AbortOnConnectFail = false,
-            }));
+        var redisConntectionString = $"{config.GetValue<string>("Redis:Host")}:{config.GetValue<int>("Redis:Port")}";
+        serviceCollection.AddSingleton<IConnectionMultiplexer>(sp =>
+            ConnectionMultiplexer.Connect(redisConntectionString));
 
         serviceCollection.AddTransient(sp => sp.GetRequiredService<IConnectionMultiplexer>().GetDatabase());
         serviceCollection.AddTransient<IDbContext, RedisDbContext>();
