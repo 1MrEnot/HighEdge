77eb2f4fc9948dec8b4f2a7ae31a57cbed0c2cd1
maxim.eremeev <maxim9236@yandex.ru>
2022-03-12 14:55:09 +0300

    Changes secret part

---
+++ Straonit.HighEdge.Core/Secret/PartOfSecret.cs
@@ -0,0 +1,16 @@
+﻿namespace Straonit.HighEdge.Core.Secret;
+
+using System.Numerics;
+
+public class PartOfSecret
+{
+    public PartOfSecret(int x, BigInteger y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public int X { get; }
+
+    public BigInteger Y { get; }
+}
